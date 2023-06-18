6d39b6806d2a9122cc04e64e83e09d64811ab1f8
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-06-18 16:22:38 -0500

    add cv file and button in navbar

--- components/site-navbar.tsx
+++ components/site-navbar.tsx
@@ -9,37 +9,17 @@ import {
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
 import { socialList } from "@/data";
+import { routes } from "@/lib/routes";
 import Link from "next/link";
-import { Menu2 } from "tabler-icons-react";
+import { Download, Menu2 } from "tabler-icons-react";
 import { NavLink } from "./navlink";
 import { buttonVariants } from "./ui/button";
-import { routes } from "@/lib/routes";
 
 export function Navbar() {
   return (
     <header className="bg-slate-900/50 backdrop-blur-md px-2 py-4 sticky top-0 z-10 border-b dark:border-b-slate-700/25">
       <MobileMenu />
-
-      <div className="hidden sm:flex container w-full flex-row justify-between">
-        <nav>
-          <ul className="flex gap-4 [&_a]:font-bold [&_a]:text-sm">
-            {routes.map((route) => (
-              <li key={route.path}>
-                <NavLink href={route.path}>{route.name}</NavLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <ul className="flex gap-3 items-center">
-          {socialList.map((s) => (
-            <li key={s.label} className="hover:text-blue-600 text-muted-foreground">
-              <a href={s.url} target="_blank" rel="noopener noreferrer">
-                <s.icon size={22} />
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <DesktopMenu />
     </header>
   );
 }
@@ -57,12 +37,17 @@ function MobileMenu() {
             <NavigationMenuContent>
               <ul className="grid grid-cols-1 w-[300px] gap-3 p-2 md:w-[500px] lg:w-[600px] ">
                 {routes.map((route) => (
-                  <li key={route.path} className="flex p-2 border-b last:border-0">
+                  <li key={route.path} className="flex p-2 border-b">
                     <Link href={route.path} legacyBehavior passHref>
                       <NavigationMenuLink className="flex-1">{route.name}</NavigationMenuLink>
                     </Link>
                   </li>
                 ))}
+                <li>
+                  <a href="/cv.pdf" className="flex-1 p-2" target="_blank" rel="noopener noreferrer">
+                    Resume
+                  </a>
+                </li>
                 <li className="flex justify-between gap-3">
                   {socialList.map((s) => (
                     <a
@@ -81,6 +66,37 @@ function MobileMenu() {
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
+    </div>
+  );
+}
+
+function DesktopMenu() {
+  return (
+    <div className="hidden sm:flex container w-full flex-row justify-between">
+      <nav>
+        <ul className="flex gap-4 [&_a]:font-bold [&_a]:text-sm">
+          {routes.map((route) => (
+            <li key={route.path}>
+              <NavLink href={route.path}>{route.name}</NavLink>
+            </li>
+          ))}
+          <li>
+            <a href="/cv.pdf" className="text-muted-foreground px-2 py-2" target="_blank" rel="noopener noreferrer">
+              Cv
+              <Download className="inline-block ml-1" size={18} />{" "}
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <ul className="flex gap-3 items-center">
+        {socialList.map((s) => (
+          <li key={s.label} className="hover:text-blue-600 text-muted-foreground">
+            <a href={s.url} target="_blank" rel="noopener noreferrer">
+              <s.icon size={22} />
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
