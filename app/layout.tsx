4f4ca5d1dd33d9bd49ebef176db9ddd4a3cc7511
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-06-18 12:19:25 -0500

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Footer } from "@/components/site-footer";
 import { Navbar } from "@/components/site-navbar";
 import { siteConfig } from "@/config/site";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Navbar />
         <main className="container py-6">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
