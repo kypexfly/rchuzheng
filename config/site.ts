3d3911bccf3034ba4f3692816c354ca56ac105e1
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-06-18 14:55:58 -0500

    update site config

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ import { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
   title: "Ricardo Chu",
-  description: "Frontend Web developer portfolio and blog",
+  description: "Web developer portfolio and blog",
   url: "https://www.ricardochu.com",
-  logo: "/icon.png",
+  logo: "/icon.svg",
 };
