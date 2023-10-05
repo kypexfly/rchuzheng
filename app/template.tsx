823db7e3be4c260403cf363df29fdb69718dc1a3
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-10-05 12:22:49 -0500

    remove template

--- app/template.tsx
+++
@@ -1,5 +0,0 @@
-import { RouteAnimate } from "@/components/route-animate";
-
-export default function RootTemplate({ children }: { children: React.ReactNode }) {
-  return <RouteAnimate>{children}</RouteAnimate>;
-}
